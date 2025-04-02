01201976a5e5b0845427bce3ee734a47c39a4653
KrishnaSonawane8008 <krishnasonawane1624@gmail.com>
2025-04-03 03:36:35 +0530

    Assignment2 Animation Synchronization

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,157 @@
-import {Canvas} from "@react-three/fiber"
+import {Canvas, useFrame} from "@react-three/fiber"
 import './App.css'
+import {useState, useEffect, useRef } from "react"
 import {OrbitControls} from "@react-three/drei"
-import Soilder from "./Soilder.jsx"
+import Avatar from "./Avatar.jsx"
+import { LoopOnce, Vector3 } from "three"
 
 
 
 
 function App() {
+  
+  
+  const [def, setDef]=useState( null )
+  const [atk, setAtk]=useState( null )
+  
+  const [def_anim, setDefenceAnimation]=useState("defence_03")
+  const [atk_anim, setAttackAnimation]=useState("attack_04")
+
+  const [def_delay, setDefenceDelay]=useState(900)
+  const [atk_delay, setAttackDelay]=useState(0)
+  const [loop_time, setLoopTime]=useState(3000)
+
+  const [attacker_pos, setAttackerPosition]=useState([0,0,14])
+  const [defender_pos, setDefenderPosition]=useState([0,0,0])
+
+  const [animation_reset, setAnimationRESET]=useState(false)
+
+  const [status, setStatus]=useState("...Wait")
+  function set_defence(self_ref){
+    setDef( ()=>self_ref )
+  }
+  function set_attack(self_ref){
+    setAtk( ()=>self_ref )
+  }
+  const Interval=useRef(null)
+  const attackTimeout=useRef(null)
+  const defenceTimeout=useRef(null)
+  useEffect( ()=>{
+    console.log("in useEffect")
+    clearInterval(Interval.current)
+    clearTimeout(defenceTimeout.current)
+    clearTimeout(attackTimeout.current)
+    if(def!=null && atk!=null){
+      
+      
+      Interval.current=setInterval( ()=>{
+        setStatus("Loop Started")
+          def.stop()
+          atk.stop()
+
+          def.reset()
+          atk.reset()
+          
+          def.clampWhenFinished=true
+          atk.clampWhenFinished=true
+          
+          def.setLoop(LoopOnce, 0)
+          atk.setLoop(LoopOnce, 0)
+
+          defenceTimeout.current=setTimeout( ()=>{
+            def.play()
+          }, def_delay )
+
+          attackTimeout.current=setTimeout( ()=>{
+            atk.play()
+          }, atk_delay )
+          
+
+      },loop_time )
+    }
+
+    return()=>{
+      clearInterval(Interval.current)
+      clearTimeout(defenceTimeout.current)
+      clearTimeout(attackTimeout.current)
+    }
+  },[def, atk, animation_reset] )
+
+
 
   return (
-    <Canvas shadows camera={ {position: [3,3,-3]} } >
-
+    <>
+    <Canvas shadows camera={ {position: [13,13,-13]} } >
+      
       <directionalLight position={ [1,2,-2] }  castShadow/>
       <ambientLight/>
-      <Soilder position={[1.5,0,0]} animation_Type={"Idle"}/>
-      <Soilder position={[0,0,0]} animation_Type={"Run"}/>
-      <Soilder position={[-1.5,0,0]} animation_Type={"Walk"}/>
+      
 
-      <mesh position={[0,-0.05,0.0]} receiveShadow>
-        <boxGeometry args={[10,0.1,10]}/>
+      <Avatar  pos={defender_pos}   pcallback={set_defence} animation={def_anim} rotation={[0,0,0]}/>
+      <Avatar  pos={attacker_pos}  pcallback={set_attack} animation={atk_anim} rotation={[0,3.14159,0]}/>
+
+      <mesh position={[0, 0.0,0.0]} rotation={[-1.5708,0,0]} receiveShadow>
+        <planeGeometry args={[100,100]}/>
         <meshStandardMaterial color={"white"}/>
       </mesh>
+
       <OrbitControls dampingFactor={1.5} />
 
     </Canvas>
+    <div className="controls">
+      <button onClick={ ()=>{ 
+        setDefenceAnimation( "defence_03" )
+        setAttackAnimation( "attack_04" )
+        setDefenceDelay(900)
+        setAttackDelay(0)
+        setLoopTime(3000)
+        setAttackerPosition([0,0,14])
+        setDefenderPosition([0,0,0])
+        setAnimationRESET((val)=>!val)
+        setStatus("...Wait")
+        } }>D3-A4</button> 
+
+      <button onClick={ ()=>{ 
+        setDefenceAnimation( "defence_03" )
+        setAttackAnimation( "attack_03" )
+        setDefenceDelay(900)
+        setAttackDelay(0)
+        setLoopTime(4000)
+        setAttackerPosition([0,0,20])
+        setDefenderPosition([0,0,0])
+        setAnimationRESET((val)=>!val)
+        setStatus("...Wait")
+        } }>D3-A3</button> 
+
+      <button onClick={ ()=>{ 
+        setDefenceAnimation( "defence_04" )
+        setAttackAnimation( "attack_02" )
+        setDefenceDelay(800)
+        setAttackDelay(800)
+        setLoopTime(3000)
+        setAttackerPosition([-1,0,3.5])
+        setDefenderPosition([0,0,0])   
+        setAnimationRESET((val)=>!val)
+        setStatus("...Wait")    
+      } }>D4-A2</button> 
+
+      <button onClick={ ()=>{ 
+        setDefenceAnimation( "defence_02" )
+        setAttackAnimation( "attack_01" )
+        setDefenceDelay(200)
+        setAttackDelay(0)
+        setLoopTime(3000)
+        setAttackerPosition([0,0,17])
+        setDefenderPosition([4,0,0])
+        setAnimationRESET((val)=>!val)
+        setStatus("...Wait")
+        } }>D2-A1</button> 
+      
+    </div>
+    <div className="Status">
+    <label>Status: {status}</label>
+    </div>
+    </>
   )
 }
 
--- src/Soilder.jsx
+++ src/Soilder.jsx
@@ -1,37 +1,93 @@
 import React, { useEffect } from 'react'
-import { useGraph } from '@react-three/fiber'
+import { useFrame, useGraph } from '@react-three/fiber'
 import { useGLTF, useAnimations } from '@react-three/drei'
 import { SkeletonUtils } from 'three-stdlib'
+import * as THREE from 'three'
 
 export default function Soilder(props) {
   const group = React.useRef()
+  const boxref=React.useRef()
   const { scene, animations } = useGLTF('/SoilderAssets/Soilder.gltf')
   const clone = React.useMemo(() => SkeletonUtils.clone(scene), [scene])
   const { nodes, materials } = useGraph(clone)
   const { actions } = useAnimations(animations, group)
   
   useEffect(()=>{
-    actions[props.animation_Type].play()
+    actions[props.animation_Type].play().fadeIn(0.5)
+
+    if(props.animation_Type=="Run"){
+
+      group.current.position.set(new THREE.Vector3(0,0,0))
+      
+      // console.log(group.current.matrixWorld.elements[2])
+      // console.log(group.current.matrixWorld.elements[6])
+      // console.log(group.current.matrixWorld.elements[10])
+    }
+
+    // world_matrix={
+    //   x-axis: 0,4,8
+    //   y-axis: 1,5,9
+    //   z-axis: 2,6,10
+    // }
+
   },[])
 
+  useFrame(
+    (state, delta, frame)=>{
+      if(props.animation_Type=="Run"){
+
+        group.current.position.set(0,0,0)
+        group.current.rotation.x-=0.01;
+        let basis=group.current.matrixWorld.extractBasis( 
+          new THREE.Vector3(1,0,0),
+          new THREE.Vector3(0,1,0),
+          new THREE.Vector3(0,0,1)
+         )
+        let Up_axis=new THREE.Vector3(basis.elements[1],
+                              basis.elements[5],
+                              basis.elements[9]).normalize()
+        boxref.current.position.set(-Up_axis.x, Up_axis.y, Up_axis.z)
+        // group.current.position.set(Up_axis.x, Up_axis.y, Up_axis.z)
+        // let new_dir_vec=new THREE.Vector3(0,2,0).applyAxisAngle(new THREE.Vector3(1,0,0), 
+        // new THREE.Vector3(0,2,0).angleTo(new THREE.Vector3(
+        //                                                     group.current.matrixWorld.elements[1],
+        //                                                     group.current.matrixWorld.elements[5],
+        //                                                     group.current.matrixWorld.elements[9])))
+
+        // group.current.position.set(new_dir_vec.x, new_dir_vec.y, new_dir_vec.z)
+        
+      }
+    }
+  )
+
   return (
+    <>
+        <mesh ref={boxref}>
+          <boxGeometry args={[0.2,0.2,0.2]}/>
+          <meshStandardMaterial color={"lightgreen"}/>
+        </mesh>
         <group ref={group} position={props.position} dispose={null} name="Character" rotation={[-Math.PI / 2, 0, 0]} scale={0.01}>
           <primitive object={nodes.mixamorigHips} />
+          
           <skinnedMesh 
             name="vanguard_Mesh" 
             geometry={nodes.vanguard_Mesh.geometry} 
             material={materials.VanguardBodyMat} 
             skeleton={nodes.vanguard_Mesh.skeleton} 
             castShadow
-            receiveShadow/>
+            receiveShadow>
+          </skinnedMesh>
+
           <skinnedMesh 
             name="vanguard_visor" 
             geometry={nodes.vanguard_visor.geometry} 
             material={materials.Vanguard_VisorMat} 
             skeleton={nodes.vanguard_visor.skeleton}
             castShadow 
-            receiveShadow/>
+            receiveShadow>
+          </skinnedMesh>
         </group>
+    </>
   )
 }
 
